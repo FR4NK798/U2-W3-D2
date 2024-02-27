6c2d0a5751790c3d04e54f8e7b18e4dcdb550522
FR4NK798 <frankricciardi98@gmail.com>
2024-02-27 18:00:05 +0100

    commit

--- index.js
+++ index.js
@@ -6,46 +6,13 @@ const form = document.querySelector("form");
 const erase = document.getElementById("eraseLast");
 const h3 = document.querySelector("h3");
 
-// const arraySavedName = [];
-
 const save = (e) => {
   e.preventDefault();
-
-  //   console.log("Array dove si salveranno i nomi: ", arraySavedName);
 
   const nameInserito = input.value;
   console.log("Nome inserito: ", nameInserito);
 
-  //   arraySavedName.push(nameInserito);
-  //   console.log("Array dopo che si è salvato un nome: ", arraySavedName);
-
   localStorage.setItem(nameKey, nameInserito);
-
-  //   alert("contenuto salvato");
-  //   localStorage.getItem(nameKey, JSON.parse(arraySavedName));
-  //   JSON.parse
-
-  // prendere da local storage e vederli
-
-  //   if (nameInserito) {
-  //     const datiSalvati = localStorage.getItem(nameKey);
-  //     console.log("dati salvati local storage: ", datiSalvati);
-  // trasformare local storage in array
-  //     const newArray = datiSalvati.split(",");
-  //     console.log("dati salvati trasformati in array, forseeee: ", newArray);
-  //   }
-
-  //   localStorage.removeItem("nameKey");
-
-  // JSON.parse(savedEvents);
-
-  //   if (arraySavedName) {
-  //     const nameSaved = document.createElement("p");
-  //     nameSaved.innerText = nameInserito;
-  //     insert.appendChild(nameSaved);
-  //   } else {
-  //     console.log("ancora non esistente");
-  //   }
 
   rivela();
 };
