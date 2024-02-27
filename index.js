e971386863da13cd69b1010aec326115555f15f2
FR4NK798 <frankricciardi98@gmail.com>
2024-02-27 17:59:22 +0100

    commit

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const input = document.querySelector("input");
 const nameKey = "nameKey";
 const form = document.querySelector("form");
 const erase = document.getElementById("eraseLast");
+const h3 = document.querySelector("h3");
 
-const arraySavedName = [];
+// const arraySavedName = [];
 
 const save = (e) => {
   e.preventDefault();
 
-  console.log("Array dove si salveranno i nomi: ", arraySavedName);
+  //   console.log("Array dove si salveranno i nomi: ", arraySavedName);
 
   const nameInserito = input.value;
   console.log("Nome inserito: ", nameInserito);
 
-  arraySavedName.push(nameInserito);
-  console.log("Array dopo che si è salvato un nome: ", arraySavedName);
+  //   arraySavedName.push(nameInserito);
+  //   console.log("Array dopo che si è salvato un nome: ", arraySavedName);
 
-  localStorage.setItem(nameKey, arraySavedName);
+  localStorage.setItem(nameKey, nameInserito);
 
   //   alert("contenuto salvato");
   //   localStorage.getItem(nameKey, JSON.parse(arraySavedName));
@@ -26,40 +27,48 @@ const save = (e) => {
 
   // prendere da local storage e vederli
 
-  if (arraySavedName) {
-    const datiSalvati = localStorage.getItem(nameKey);
-    console.log("dati salvati local storage: ", datiSalvati);
-    // trasformare local storage in array
-    const newArray = datiSalvati.split(",");
-    console.log("dati salvati trasformati in array, forseeee: ", newArray);
-  }
+  //   if (nameInserito) {
+  //     const datiSalvati = localStorage.getItem(nameKey);
+  //     console.log("dati salvati local storage: ", datiSalvati);
+  // trasformare local storage in array
+  //     const newArray = datiSalvati.split(",");
+  //     console.log("dati salvati trasformati in array, forseeee: ", newArray);
+  //   }
 
   //   localStorage.removeItem("nameKey");
 
   // JSON.parse(savedEvents);
 
-  if (arraySavedName) {
-    const nameSaved = document.createElement("p");
-    nameSaved.innerText = nameInserito;
-    insert.appendChild(nameSaved);
+  //   if (arraySavedName) {
+  //     const nameSaved = document.createElement("p");
+  //     nameSaved.innerText = nameInserito;
+  //     insert.appendChild(nameSaved);
+  //   } else {
+  //     console.log("ancora non esistente");
+  //   }
+
+  rivela();
+};
+
+window.onload = () => {
+  rivela();
+};
+
+const rivela = () => {
+  const datoSalvato = localStorage.getItem(nameKey);
+  console.log(datoSalvato);
+  if (datoSalvato) {
+    h3.innerText = datoSalvato;
   } else {
-    console.log("ancora non esistente");
+    h3.innerText = "";
   }
 };
 
-const eraseLocal = (form) => {
-  console.log("form", form);
-  console.log("funziona");
-  console.log(arraySavedName);
-  const elimina = arraySavedName.pop();
-  console.log("const elimina", elimina);
-  console.log("arraySavedName", arraySavedName);
-
-  localStorage.setItem(nameKey, arraySavedName);
-
-  //   nameSaved.innerText = "eliminato";
+const eraseLocal = () => {
+  localStorage.removeItem(nameKey);
+  rivela();
 };
 
 form.addEventListener("submit", save);
 
-erase.addEventListener("click", eraseLocal(form));
+erase.addEventListener("click", eraseLocal);
